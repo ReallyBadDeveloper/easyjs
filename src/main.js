310552fd8718dae26677ae4fec1b4f1d1967c8f2
Benjamin M <reallybaddeveloper@gmail.com>
2024-05-03 12:02:23 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
-var wait = (time,function) => {
+var wait = (time=1000,function=() => {console.error('Uncaught TypeError: Function must be defined.')}) => {
   setTimeout(function(),time);
 };
+var loopCode = (iter,)
